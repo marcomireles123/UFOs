99d3eb7da4f5f5af11a8355ebd307b95395c3430
Marco <marco_mireles@outlook.com>
2022-12-01 10:47:57 -0600

    practice

--- Practice/Syntax.js
+++ Practice/Syntax.js
@@ -5,3 +5,6 @@ console.log('Hello World!');
 //Assign a variable
 var y=2;
 console.log(y); 
+
+// 'let' variables
+// This is a scenario where it might be used: "I might want to use this variable again later to hold different data, but in this code block I'll only use it once."
