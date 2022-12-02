ac384ccf115415587f57b9739bf22c695e6775e0
Marco <marco_mireles@outlook.com>
2022-12-01 19:17:31 -0600

    buildTable function

--- JS/app.js
+++ JS/app.js
@@ -10,10 +10,22 @@ function buildTable(data) {
 
 // Clear existing data
 function buildTable(data) {
+    // First, clear out any existing data
     tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
   }
 
-  // Add 'forEach' function
-  data.forEach((dataRow) => {
-
-});
