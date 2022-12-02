874fc6a75a7ff84bec9d998a5850832a34d339b1
Marco <marco_mireles@outlook.com>
2022-12-01 19:09:51 -0600

    cleared existing data

--- JS/app.js
+++ JS/app.js
@@ -3,3 +3,12 @@ const tableData = data;
 
 //Reference the HTML table using D3
 var tbody = d3.select('tbody');
+
+function buildTable(data) {
+
+}
+
+// Clear existing data
+function buildTable(data) {
+    tbody.html("");
+  }
