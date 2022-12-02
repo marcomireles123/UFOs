bac6b9c3ac0ef33923b3b3966e58f3ad01d2fe25
Marco <marco_mireles@outlook.com>
2022-12-01 23:00:55 -0600

    looped through all the filters

--- JS/app.js
+++ JS/app.js
@@ -60,7 +60,9 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(filters).forEach(([key,value])=> {
+      filteredData =  filteredData.filter(row => row[key]=== value);
+    });
   
     // 10. Finally, rebuild the table using the filtered data
     
